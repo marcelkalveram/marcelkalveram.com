7898efa6db3e4bffa15f049fccfdbd7238a06f1e
Marcel Kalveram <marcel.kalveram@googlemail.com>
2024-07-16 11:10:39 +0200

    update projects with showreels

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -12,11 +12,11 @@ import logoNuhouse from '@/images/logos/projects/nuhouse.png'
 import clsx from 'clsx'
 import logoMvpr from '@/images/logos/projects/mvpr.png'
 import logoHanno from '@/images/logos/projects/hanno.png'
-import logoDelusha from '@/images/logos/projects/delusha.png'
+import logoOmron from '@/images/logos/projects/omron.png'
 import logoMytheresa from '@/images/logos/projects/mytheresa.png'
 import logoUncovercity from '@/images/logos/projects/uncovercity.png'
 import logoPrepd from '@/images/logos/projects/prepd.png'
-import logoNcube from '@/images/logos/projects/ncube.png'
+import logoGermanDoctors from '@/images/logos/projects/german-doctors.png'
 import logoAva from '@/images/logos/projects/ava.png'
 import logoUnicef from '@/images/logos/projects/unicef.png'
 
@@ -28,6 +28,10 @@ const projectsWork = [
     description:
       "As the lead developer, I spearheaded the creation of the web app's initial prototype and core features using Firebase and Tailwind UI. After validating the problem/solution fit, we aimed to solve the most common inefficiencies in PR by building AI-powered solutions.",
     logo: logoMvpr,
+    link: {
+      label: 'Go to website',
+      href: 'https://mvpr.io',
+    },
   },
   {
     name: 'Hanno',
@@ -36,6 +40,10 @@ const projectsWork = [
     description:
       'As part of a team of two engineers and one UX/UI designer, I helped develop two mobile healthcare apps in React Native. I had to thoroughly follow extensive unit and e2e testing best practices to comply with quality standards in the strictly regulated healthcare sector.',
     logo: logoHanno,
+    link: {
+      label: 'Go to website',
+      href: 'https://hanno.co',
+    },
   },
   {
     name: 'mytheresa',
@@ -44,6 +52,10 @@ const projectsWork = [
     description:
       "After leading a one-week workshop for the dev team to get proficient in React Native, I helped migrate the company's native iOS/Android app to it, integrating a reliable CI/CD pipeline, establishing unit and e2e testing best practices, and upskilling individual contributors.",
     logo: logoMytheresa,
+    link: {
+      label: 'See on Apple app store',
+      href: 'https://apps.apple.com/us/app/mytheresa-shop-luxury-brands/id484615570?platform=iphone',
+    },
   },
   {
     name: 'uncovercity',
@@ -52,6 +64,11 @@ const projectsWork = [
     description:
       'Working alongside a UX/UI designer and backend developer, I implemented the mobile app for this surprise dinner startup. Part of the challenge was integrating the API for a third-party car-sharing service and displaying real-time updates to the user.',
     logo: logoUncovercity,
+    link: {
+      label: 'Read blog post series',
+      href: '/articles/2018-09-27-speeding-up-the-build-of-a-surprise-dinner-app-in-react-native-using-expo',
+      blog: true,
+    },
   },
   {
     name: 'prepd',
@@ -60,22 +77,49 @@ const projectsWork = [
     description:
       'When our client launched their physical lunch box product, supported by over 40,000 people through Kickstarter, I was part of the team that shipped the accompanying recipe app within six months. Using React Native, we were able to accelerate the process significantly.',
     logo: logoPrepd,
+    link: {
+      label: 'Watch the showreel',
+      href: 'https://vimeo.com/203132849',
+      showreel: true,
+    },
+  },
+  {
+    name: 'Omron EVOLV',
+    year: '2016',
+    tags: ['Web', 'node.js', 'Pub/Sub'],
+    description:
+      'I was the lead front-end developer of a team of three that built the marketing website of the new Omron EVOLV blood pressure monitor. We built and shipped the site—which features an interactive and immersive product walkthrough—in just four months.',
+    logo: logoOmron,
+    link: {
+      label: 'Watch the showreel',
+      href: 'https://vimeo.com/233500897',
+      showreel: true,
+    },
   },
   {
     name: 'Ava',
     year: '2015',
     tags: ['Web', 'node.js', 'Pub/Sub'],
     description:
-      'Ava is an app for the Deaf and hard of hearing that enables high-accuracy 24/7 real-time captions. I was part of the team developing the web-based front end, which uses a light API layer to connect to other participants and allows people to participate in conversations from any browser.',
+      'Ava is an app for the Deaf and hard of hearing that enables high-accuracy 24/7 real-time captions. I was part of the team developing the web-based front end, which uses a light API layer to and allow people to participate in conversations from any browser.',
     logo: logoAva,
+    link: {
+      label: 'Go to website',
+      href: 'https://www.ava.me/',
+    },
   },
   {
-    name: 'nCube Home',
-    year: '2014',
-    tags: ['Mobile', 'AngularJS'],
+    name: 'German Doctors',
+    year: '2013',
+    tags: ['Web', 'Responsive Design', 'Contao CMS'],
     description:
-      'I helped develop an early prototype for an Intelligent Smart Home platform called nCube, a pioneer in IoT platform development. To quickly build the app, we used Angular Mobile and created an API layer to communicate with the nCube hub.',
-    logo: logoNcube,
+      "I was the technical project manager at Cologne's digital web agency JUNGMUT when German Doctors asked us to redesign their website. With a small team of three developers, we rebuilt the entire site and made it accessible on mobile devices for the first time.",
+
+    logo: logoGermanDoctors,
+    link: {
+      label: 'Go to website',
+      href: 'https://www.german-doctors.de/',
+    },
   },
   {
     name: 'UNICEF',
@@ -85,15 +129,19 @@ const projectsWork = [
       "As part of a team of two developers, I built a web app for the International Day of Children's Rights. The app featured an interactive map where children and schools could voice their opinions. We shipped this challenging project on a tight deadline of just one month.",
 
     logo: logoUnicef,
+    link: {
+      label: 'Go to Awwwards submission',
+      href: 'https://www.awwwards.com/sites/unicef-aktionstag-kinderrechte',
+    },
   },
-  {
-    name: 'Delusha',
-    year: '2010',
-    tags: ['Web', 'CakePHP', 'jQuery'],
-    description:
-      "I developed a complete eCommerce shop for custom-made jewellery from scratch in CakePHP, integrating a jewellery design tool created in Adobe Flash. I also set up the back-office and user management interface using CakePHP's scaffolding capabilities.",
-    logo: logoDelusha,
-  },
+  // {
+  //   name: 'Delusha',
+  //   year: '2010',
+  //   tags: ['Web', 'CakePHP', 'jQuery'],
+  //   description:
+  //     "I developed a complete eCommerce shop for custom-made jewellery from scratch in CakePHP, integrating a jewellery design tool created in Adobe Flash. I also set up the back-office and user management interface using CakePHP's scaffolding capabilities.",
+  //   logo: logoDelusha,
+  // },
 ]
 const projectsPersonal = [
   {
@@ -147,7 +195,7 @@ const projectsPersonal = [
   {
     name: 'nuhouse.de',
     year: '2004',
-    tags: ['Wordpress', 'phpBB'],
+    tags: ['Web', 'Wordpress', 'phpBB'],
     description:
       'Nuhouse was an online platform and forum specialising in deep house music. Starting as a hobby project about my big passion at that time, I used WordPress and phpBB to create a little community of around 200-300 people.',
     link: {
@@ -184,6 +232,36 @@ const projectsVolunteering = [
 ]
 
 function LinkIcon(props) {
+  if (props.blog) {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="size-6"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z"
+        />
+      </svg>
+    )
+  }
+  if (props.showreel) {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        class="size-6"
+      >
+        <path d="M4.5 4.5a3 3 0 0 0-3 3v9a3 3 0 0 0 3 3h8.25a3 3 0 0 0 3-3v-9a3 3 0 0 0-3-3H4.5ZM19.94 18.75l-2.69-2.69V7.94l2.69-2.69c.944-.945 2.56-.276 2.56 1.06v11.38c0 1.336-1.616 2.005-2.56 1.06Z" />
+      </svg>
+    )
+  }
   return (
     <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
       <path
@@ -248,7 +326,7 @@ export default function Projects() {
                 {project.tags.map((tag) => (
                   <span
                     key={tag}
-                    className="py-.5 rounded-md bg-zinc-100 px-2 dark:bg-zinc-700 dark:text-zinc-300"
+                    className="py-.5 rounded-md bg-zinc-100 px-2 group-hover:opacity-95 dark:bg-zinc-700 dark:text-zinc-300"
                   >
                     {tag}
                   </span>
@@ -260,7 +338,11 @@ export default function Projects() {
 
             {project.link && (
               <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
-                <LinkIcon className="h-6 w-6 flex-none" />
+                <LinkIcon
+                  className="h-6 w-6 flex-none"
+                  showreel={project.link.showreel}
+                  blog={project.link.blog}
+                />
                 <span className="ml-2">{project.link?.label}</span>
               </p>
             )}
