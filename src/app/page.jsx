e439845d84a1895431ed9395d89f144a713ea4ee
Marcel Kalveram <marcel.kalveram@googlemail.com>
2024-09-26 21:32:08 +0200

    update tagline

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -377,10 +377,10 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            Hi! I&apos;m Marcel. 👋
+            Hi! I&apos;m Marcel 👋
             <br />
-            <span className="text-gray-400">
-              I build and improve digital products.
+            <span className="text-4xl text-gray-400">
+              Product Engineer | Tech Lead | German Expat in Spain
             </span>
           </h1>
           <p className="mt-6 max-w-2xl text-base text-zinc-600 dark:text-zinc-400">
